1ab25d04da411d0029473a7bee853e6cf04c38a4
Joshua Hubbard <jhubardsf@gmail.com>
2020-01-26 04:28:47 -0500

    Reset iExpense to original state

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -9,31 +9,9 @@
 import SwiftUI
 
 
-struct ContentView: View {
-    @State private var numbers = [Int]()
-    @State private var currentNumber = 1
-    
+struct ContentView: View {    
     var body: some View {
-        NavigationView {
-            VStack {
-                List {
-                    ForEach(numbers, id: \.self) {
-                        Text("\($0)")
-                    }
-                    .onDelete(perform: removeRows)
-                }
-                
-                Button("Add Number") {
-                    self.numbers.append(self.currentNumber)
-                    self.currentNumber += 1
-                }
-            }
-            .navigationBarItems(leading: EditButton())
-        }
-    }
-    
-    func removeRows(at offsets: IndexSet) {
-        numbers.remove(atOffsets: offsets)
+        Text("Hellloooooo")
     }
 }
 
