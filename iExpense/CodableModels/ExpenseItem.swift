0b0d1f9574a9058a01aac932aafb8bfc36ebb33d
Joshua Hubbard <jhubardsf@gmail.com>
2020-01-26 04:46:15 -0500

    Add expense list and add expense

---
+++ iExpense/CodableModels/ExpenseItem.swift
@@ -0,0 +1,10 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Josh Hubbard on 1/26/20.
+//  Copyright © 2020 YeetBox. All rights reserved.
+//
+
+import SwiftUI
+
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,10 +8,36 @@
 
 import SwiftUI
 
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
 
-struct ContentView: View {    
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
+struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    
     var body: some View {
-        Text("Hellloooooo")
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    self.expenses.items.append(expense)
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
+        }
     }
 }
 
